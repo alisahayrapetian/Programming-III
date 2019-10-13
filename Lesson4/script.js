5bd7141492a0093ffef3a9c9d90a5f72c14c3993
alisahayrapetian <alisa.h.s@tumo.org>
2019-10-13 19:03:00 +0400

    kijhgfds

---
+++ Lesson4/script.js
@@ -0,0 +1,30 @@
+// function windowLoad() {
+//     console.log("Window is loaded");
+//  }
+//  window.onload = windowLoad;
+//  // .addEventListener("onload",windowLoad)
+function main() {
+    var socket = io();
+    var chatDiv = document.getElementById('chat');
+    var input = document.getElementById('message');
+    var button = document.getElementById('submit');
+ 
+    function handleSubmit(evt) {
+        var val = input.value;
+        if (val != "") {
+            socket.emit("send message", val);
+        }
+    }
+    button.onclick = handleSubmit;
+ 
+    function handleMessage(msg) {
+        var p = document.createElement('p');
+        p.innerText = msg;
+        chatDiv.appendChild(p);
+        input.value = "";
+}
+
+socket.on('display message', handleMessage);
+} // main closing bracket
+
+window.onload = main;   
